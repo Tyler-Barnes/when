ce29135024e228634fdb5ce5ee5bf9931c5c425b
Tyler Barnes <59125882+Tyler-Barnes@users.noreply.github.com>
2024-05-26 20:03:18 -0500

    Update WHEN.h

--- WHEN.h
+++ WHEN.h
@@ -1,10 +1,4 @@
 // Tyler Barnes 2024
 // The 'when' function is what happens when you !not the condition of a while loop
-// It is only used when working on embedded programs or serial where the condition is updated by outside parameters
-// I often find myself waiting on a register flag or pinstate to change before another action is taken
-// I would write while(!digitalRead(pin)){} or similar.
-// So instead I write when (digitalRead(pin)) {//do something} 
-// I find it easier to read the logic as "when condition is true, then do the thing"
-// rather than "while condition is not true, wait to proceed"
 
 #define when(x) while(!(x)){}
